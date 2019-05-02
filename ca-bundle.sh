757f487c87edb38a67735a13e10a8a416e5f7c6d
Kyrre Havik Eriksen <kyrremann@gmail.com>
2019-05-02 07:39:28 +0200

    Lagt til riktig endepunkt for test

--- ca-bundle.sh
+++ ca-bundle.sh
@@ -8,4 +8,4 @@ ROOT=$(cd $(dirname $0)/../../; pwd)
 export CA_BUNDLE=$(kubectl get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' | base64 | tr -d '\n')
 
 sed -i "s/caBundle: .*$/caBundle: ${CA_BUNDLE}/g" ./webhook.yaml
-sed -i "s/url: .*$/url: https:\/\/$(hostname -f):8443/g" ./webhook.yaml
+sed -i "s/url: .*$/url: https:\/\/$(hostname -f):8443\/mutate/g" ./webhook.yaml
